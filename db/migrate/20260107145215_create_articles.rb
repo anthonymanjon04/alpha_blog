71780629ef3dc3966e0b1a2c076e37dd2a8178a6
Anthony Manjon <manjonanthony50@gmail.com>
2026-01-07 11:57:59 -0400

    fixing type error

--- db/migrate/20260107145215_create_articles.rb
+++ db/migrate/20260107145215_create_articles.rb
@@ -2,7 +2,7 @@ class CreateArticles < ActiveRecord::Migration[7.2]
   def change
     create_table :articles do |t|
       
-      t.string :tittle
+      t.string :title
       t.text :description
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema[7.2].define(version: 2026_01_07_145215) do
   create_table "articles", force: :cascade do |t|
-    t.string "tittle"
+    t.string "title"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
